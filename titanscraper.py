ecf7c4837bea48bfc2eb0ffb5e54e4bba47d24b2
emileKing <djidadevacc@gmail.com>
2021-10-18 09:56:51 +0100

    code refactoring

--- titanscraper.py
+++ titanscraper.py
@@ -1,3 +1,4 @@
+from typing import runtime_checkable
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,9 +12,10 @@ class TitanScraper():
     HTML_PARSER = "lxml"
 
     def __init__(self) -> None:
-        print(f"TitanScraper version {self.VERSION}")
+        pass
 
-    def __get_raw_html(self, link:str, timeout=5) -> tuple:
+    @staticmethod
+    def __get_raw_html(link:str, timeout=5) -> tuple:
         """Gets the raw html of a given link.
         
         Arguments:
@@ -32,23 +34,65 @@ class TitanScraper():
         else:
             return (response.status_code, response.content)
 
+    
+    @staticmethod
+    def __preprocess_element(element, preprocessors:list=None): 
+        """Go through all given proprocessors and apply each to the element sequentially"""
+        if not preprocessors:
+            return element
+        for processor in preprocessors:
+            element = processor(element)
+        return element
+
+
+    @staticmethod
+    def __extract_value(element, rule_attribute:str=None):
+        """Extract the given attribbute of an element or it's text content"""
+        return element.get(rule_attribute) if rule_attribute else str(element.string)
+
+
+    @staticmethod
+    def __postprocess_elememt(value, processors:list=None):
+        """Apply the given postprocessings to an element's value"""
+        if not processors:
+            return value
+        for processor in processors:
+            # check if it is a class
+            if type(processor) == type(object):
+                value = processor(value).get_value()
+            #  or an object
+            else:
+                value = processor.set_value(value).get_value()
+        return value
+
 
     def __parse_data(self, html_doc:str, rules:tuple) -> dict:
         """Parse a given html document following a given set of rules and return the data"""
         soup = BeautifulSoup(html_doc, self.HTML_PARSER)
         data = {}
         for rule in rules:
-            temp = soup.select(rule.get('selector'))
-            if temp:
-                temp = temp[0]
-                # create an entry with the rule name as key and the selected 
-                # element's attribute value of text content
-                value = temp.get(rule['attribute']) if rule.get("attribute") else str(temp.contents[0])
-                # cast the value to the given type else just leave it as string  
-                try:
-                    data[rule.get('name')] = rule['type'](value) if rule.get('type') else value
-                except:
-                    raise
+            # get the elements following the selector
+            elements = soup.select(rule.get('selector')) 
+            if elements:
+                item_index = rule.get("index", 0)
+                # get the element from the list of elements
+                element = elements[item_index]
+                # preprocess the data
+                element = self.__preprocess_element(element, rule.get("preprocessors"))
+                # extract the value or content from the element
+                value = self.__extract_value(element, rule.get('attribute'))
+                # apply data postprocessing
+                value = self.__postprocess_elememt(element, rule.get("postprocessors"))
+                # type casting
+                value = rule['type'](value) if rule.get('type') else str(value)
+
+                # for conditional evaluations
+                # this adds the value to the final result only if it meets the evaluated conditions
+                if rule.get("evaluators"):
+                    for evaluator in rule.get('evaluators'):
+                        if evaluator.evaluate(value):
+                            data[rule.get('name')] = value
+
         return data
 
 
@@ -72,20 +116,26 @@ class TitanScraper():
         """
         results = []
         for target in targets:
-            print("- processing ", target)
+            # print("- processing ", target)
             _ , raw_html = self.__get_raw_html(target)
-            if raw_html:
-                try:
-                    data = self.__parse_data(raw_html, rules)
-                    print(data)
-                except Exception as e:
-                    print(e, "occured, skipping page!")
-                else:
+            if not raw_html:
+                pass
+
+            try:
+                data = self.__parse_data(raw_html, rules)
+                # print(data)
+            except Exception as e:
+                print("[ERROR]", e, "occured, skipping page!")
+                # raise
+            else:
+                # those who failed the evaluations (if given) would not be added
+                if data:
+                    data['source'] = target
                     results.append(data)
         return results
 
     
-    def get_links_from_page(self, target, page, rule=''):
+    def get_links_from_page(self, target:str, page:str, rule:str='') -> list:
         """Get all links in a given page if no rule is passed, else based on the css selector"""
         _, raw_html = self.__get_raw_html(target+page)
         soup = BeautifulSoup(raw_html, self.HTML_PARSER)
@@ -94,6 +144,7 @@ class TitanScraper():
         else:
             return [ target + link.get('href') for link in soup.select('a') ] 
 
-    def dict_to_datalist(self, _dict:dict) -> tuple:
+    @staticmethod
+    def dict_to_datalist(_dict:dict) -> tuple:
         """returns a tuple of values from a dict"""
         return tuple( _dict.values() )
